9db5df5cd2fbf3616df3b439c700c5cd8e8575fa
Michael Muniu <muniu.dev@gmail.com>
2024-03-05 19:22:02 +0300

    lab completed

---
+++ server/migrations/versions/349ece41acc8_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 349ece41acc8
+Revises: 
+Create Date: 2024-01-11 05:14:47.672149
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '349ece41acc8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
